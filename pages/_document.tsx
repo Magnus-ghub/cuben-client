5503f8ec5155601722d5c12d6a2be981de2ea765
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-30 15:26:15 +0900

    feat: install project packages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/* SEO */}
+        <meta
+          name="keyword"
+          content={
+            "cuben, campus market, cuben.uz, student market, buy sell items, second-hand"
+          }
+        />
+
+        <meta
+          name="description"
+          content={
+            // Uzbekcha
+            "Cuben — talabalarga mo‘ljallangan second-hand marketplace. Universitet ichida kitoblar, elektronika, kiyim-kechak va boshqa buyumlarni oson sotish va xarid qilish platformasi. | " +
+            // Ruscha
+            "Cuben — студенческий маркетплейс для покупки и продажи подержанных вещей: книги, электроника, одежда и многое другое внутри кампуса. Удобно, быстро и безопасно. | " +
+            // Koreyscha
+            "Cuben — 대학생을 위한 중고 거래 플랫폼입니다. 캠퍼스 내에서 책, 전자제품, 의류 등 다양한 물품을 쉽고 안전하게 사고 팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
