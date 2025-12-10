663ffd33450980db4768cac07d1d57e073818401
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-12-10 22:25:07 +0900

    feat: create community and events section in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,28 @@ import { NextPage } from "next";
 import withLayoutMain from "../libs/components/layout/LayoutHome";
 import Advertisement from "../libs/components/homepage/Advertisement";
 import PopularProducts from "../libs/components/homepage/PopularProducts";
+import useDeviceDetect from "../libs/hooks/useDeviceDetect";
+import Community from "./community";
+import CommunityBoards from "../libs/components/homepage/Community";
+import Events from "../libs/components/homepage/Event";
 
 const Home: NextPage = () => {
+  const device = useDeviceDetect();
+
+  if (device === 'mobile') {
+    return (
+      <Stack className={'home-page'}>
+        <PopularProducts />
+      </Stack>
+    )
+  }
   return (
     <Container>
       <Stack flexDirection={"column"}>
-         <Advertisement />
          <PopularProducts />
+         <Advertisement />
+         <CommunityBoards />
+         <Events />
         <div>HomePage</div>
       </Stack>
     </Container>
