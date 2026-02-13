0087e39b13cd3cc034665ddd2ec3908d3aff6530
ilhom nomozov <nomozof@IlhomMacBook.local>
2026-02-13 23:01:51 +0900

    fix: remove twiewer scss

--- libs/components/community/TViewer.tsx
+++ libs/components/community/TViewer.tsx
@@ -4,7 +4,7 @@ import StarterKit from '@tiptap/starter-kit';
 import Link from '@tiptap/extension-link';
 import Image from '@tiptap/extension-image';
 import { Box, Stack } from '@mui/material';
-import './TViewer.scss';
+
 
 interface TViewerProps {
 	markdown?: string;
